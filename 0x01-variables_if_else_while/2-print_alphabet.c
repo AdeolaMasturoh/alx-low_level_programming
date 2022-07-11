a024b6835a1f6617362ebc1e2ff3d7ffe10a7289
Super User <Adegbolamisturag@gmail.com>
2022-07-11 15:22:28 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return(0);

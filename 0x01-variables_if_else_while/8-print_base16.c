a88a4a3dcc61468ac899060dfaea9d25f5cebea5
Super User <Adegbolamisturag@gmail.com>
2022-07-12 03:00:04 -0700

    print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:Always return 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(n);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}

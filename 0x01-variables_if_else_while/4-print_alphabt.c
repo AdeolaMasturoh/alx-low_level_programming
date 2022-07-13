8489651be82cda29a907a41791d6500a6fb87967
Super User <Adegbolamisturag@gmail.com>
2022-07-13 09:28:56 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,17 @@
  */
 int main(void)
 {
-	char letter;
+	char ch;
 
-	for (letter ='a';letter <= 'z'; letter++)
+	for (ch ='a'; ch <= 'z'; ch++)
 	{
-		putchar(letter);
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
 	}
-	for 
+
+	putchar (10);
+
+	return (0);
+}

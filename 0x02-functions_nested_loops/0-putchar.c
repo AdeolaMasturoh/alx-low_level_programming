5d715243cfda28f790c18c68bc2de6b95ec36137
Super User <Adegbolamisturag@gmail.com>
2022-07-13 13:14:12 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: return success
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file for alx c low level programming function, nested loop

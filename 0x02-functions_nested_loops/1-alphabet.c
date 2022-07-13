5eaaf9c8a70adb74fcd0f48b83a69ea090bba9ab
Super User <Adegbolamisturag@gmail.com>
2022-07-13 13:41:06 -0700

    prints lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints alphabets in lower case
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}

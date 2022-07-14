a74014458a59605b39cb4e944bde348b512feb34
Super User <Adegbolamisturag@gmail.com>
2022-07-14 03:37:00 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all multiplies of 3 and 5 below 1024
+ * Return: always success
+ */
+int main(void)
+{
+	unsigned long int sun3, sum5, sum;
+
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (o);
+}
